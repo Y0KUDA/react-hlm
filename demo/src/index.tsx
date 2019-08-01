d453d9ccfdd4f4cb931e88d888feff79bccafc9e
Y0KUDA <40825115+Y0KUDA@users.noreply.github.com>
2019-08-02 04:50:22 +0900

    add link

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -5,6 +5,7 @@ import HLM from './react-hlm';
 const Demo:React.FC = () => {
     return <>
     <div style={{display: 'flex', justifyContent: 'center'}}><HLM.PopText text="Hi! This is react-hlm demo page." distanceMax={0.5} distanceMin={0.2} style={{fontSize:"40px"}}/></div>
+    <div style={{display: 'flex', justifyContent: 'center'}}><a href="https://github.com/Y0KUDA/react-hlm"><HLM.Banner text="https://github.com/Y0KUDA/react-hlm" distanceMax={0.8} spring="normal" waveSpeed={0.8} distanceMin={0.2} style={{fontSize:"40px"}}/></a></div>
 
     <div style={{display: 'flex', justifyContent: 'center'}}>HLM.Banner spring="none"</div>
     <div style={{display: 'flex', justifyContent: 'center'}}><HLM.Banner spring="none"/></div>
